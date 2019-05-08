cedb9564b8ec31ac8fea712de7dc860738d64617
Ivoteki <ivotek.gamer@live.com.pt>
2019-05-08 15:53:34 +0100

    Final_S6

--- Estagio-Teste/src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ Estagio-Teste/src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -12,7 +12,7 @@ export class ShoppingCartService{
     addItem(item:MenuItem){
         let foundItem = this.items.find((mItem)=> mItem.menuItem.id === item.id)
         if(foundItem){
-            foundItem.quantity = foundItem.quantity + 1
+            this.increaseQty(foundItem)
         }else{
             this.items.push(new CartItem(item))
         }
@@ -22,6 +22,20 @@ export class ShoppingCartService{
 
         this.items.splice(this.items.indexOf(item), 1)
     }
+
+    increaseQty(item: CartItem ){
+        item.quantity = item.quantity + 1
+    }
+
+    decreaseQty(item: CartItem){
+
+        item.quantity = item.quantity - 1
+        if(item.quantity === 0){
+            this.removeItem(item)
+        }       
+    }
+    
+
 
     total(): number{
         return this.items.map(item => item.value()).reduce((prev, value)=> prev + value, 0)
